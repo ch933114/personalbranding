47740e3591359c0aa70541ae6d5faf4f9dd3c2ac
Dean <41066546+ch933114@users.noreply.github.com>
2025-05-14 14:41:26 +0800

    初次上板

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',        // HTML 檔案
+    './blog.html',
+    './blogDetail.html',
+    './app.js',            // JS 檔案
+    './styles.css',        // 如果你有單獨的 CSS 檔案
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
